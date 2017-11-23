d24f0599155fc1058443d4f2df4d8a816c5a6f8f
zhangfangliZFL <1252709855@qq.com>
2017-11-23 22:13:40 +0800

    zfl

--- src/components/car/carFooter.vue
+++ src/components/car/carFooter.vue
@@ -1,37 +1,88 @@
 <template>
 	<div class="car-footer">
 	 	<div class="car-checked">
-	 		<input type="redio" checked>
+	 		<input class="sectle" type="radio" checked>
 	 		<span>全选</span>
 	 	</div>
 	 	<div class="car-info">
-	 		<div>
-	 			<span>总计:￥ （1件）</span>
-	 			<span>不含运费</span>
+	 		<div class="car-price">
+	 			<span>总计:￥{{all_price_num.price}} （{{all_price_num.num}}件）</span>
+	 			<span class="freight">不含运费</span>
 	 		</div>
-	 		<div>
-	 			<button>结算</button>
+	 		<div class="my-btn">
+	 			<button class="btn">结算</button>
 	 		</div>
 	 	</div>
 	</div>
 </template>
 
 <script>
-	export default{
-		name:"carFooter",
-		data:function(){
-			return {
+import { mapGetters } from 'vuex'
+export default{
+	name:"carFooter",
+	data:function(){
+		return {
 
-			}
 		}
-	}
+	},
+	computed:{
+   
+    	...mapGetters(['pureCar','all_price_num'])
+    },
+}
 </script>
 
 <style scoped lang="scss">
 	.car-footer{
+		box-sizing:border-box;
 		width:100%;
 		height:0.6rem;
 		display:flex;
 		justify-content:space-between;
+		border:1px solid #ccc;
+	}
+	.car-checked{
+		padding-left:0.1rem;
+		width:0.45rem;
+		height:0.6rem;
+		display:flex;
+		flex-direction:column;
+		justify-content:center;
+		
+		
+	}
+
+	.car-info{
+		width:2.3rem;
+		height:0.6rem;
+		display:flex;
+		justify-content:space-between;
+	}
+	.car-price{
+		display:flex;
+		flex-direction:column;
+		justify-content:center;
+		color:#d0253b;
+	
+	}
+	.freight{
+		color:#ccc;
+		font-size:0.12rem;
+		padding-top:0.05rem;
+		padding-left:0.4rem;
+	}
+	.my-btn{
+		padding-right:0.1rem;
+		line-height:0.6rem;
+	}
+	.btn{
+
+		background:#d0253b;
+		border:none;
+    	width: 0.85rem;
+    	height: 0.42rem;
+    	border-radius: .05rem;
+    	color:#fff;
+    	font-size:0.16rem;
 	}
 </style>
